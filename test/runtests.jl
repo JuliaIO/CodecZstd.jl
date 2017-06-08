9f6c52269727cda619c867d93cdb64d7cdb819e0
Kenta Sato <bicycle1885@gmail.com>
2017-06-08 23:25:44 +0900

    remove a test data file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,6 @@ using CodecZstd
 using Base.Test
 import TranscodingStreams
 
-const testdir = dirname(@__FILE__)
-
 @testset "Zstd Codec" begin
     data = [0x28, 0xb5, 0x2f, 0xfd, 0x04, 0x50, 0x19, 0x00, 0x00, 0x66, 0x6f, 0x6f, 0x3f, 0xba, 0xc4, 0x59]
     @test read(ZstdDecompressionStream(IOBuffer(data))) == b"foo"
