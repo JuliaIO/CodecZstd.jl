ad7288a66afc56e30db9a5b276c46c182d881ee7
Mark Kittisopikul <mkitti@users.noreply.github.com>
2024-05-19 18:32:57 -0400

    Use ZSTD_compressStream2 with endOp argument (#49)
    * Change compress! to use ZSTD_compressStream2 rather than ZSTD_compressStream
    
    * Add tests for endOp
    
    * Add new test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,4 +43,6 @@ Random.seed!(1234)
     TranscodingStreams.test_roundtrip_write(ZstdCompressorStream, ZstdDecompressorStream)
     TranscodingStreams.test_roundtrip_lines(ZstdCompressorStream, ZstdDecompressorStream)
     TranscodingStreams.test_roundtrip_transcode(ZstdCompressor, ZstdDecompressor)
+
+    include("compress_endOp.jl")
 end
